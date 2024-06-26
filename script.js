1045cf592e28d0ae82b9585fcdaf437368466af1
MFbomboclat <dylanmf1305@gmail.com>
2024-06-26 09:40:52 -0700

    Listo para revision

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 const tar = document.getElementById("add");
-            tar.addEventListener("click",addTarea);
-            function addTarea() {
-                let t = document.getElementById("taskInput").value;
-                document.getElementById("task").innerHTML+=
-                "<li><h3><input type='checkbox'>"+t+"</h3></li>";
-            }
-            const del = document.getElementById("delete");
-            del.addEventListener("click",delTarea);
-            function delTarea() {
-                let t = document.getElementById("task");
-                let lTask = t.lastChild;
-                t.removeChild(lTask);
-            }
+tar.addEventListener("click",addTarea);
+function addTarea() {
+    let t = document.getElementById("taskInput").value;
+    if (t!="") {
+        document.getElementById("task").innerHTML += 
+        "<li><h3>"+t+"<input type='checkbox'></input></h3></li>";
+    }
+}
+const del = document.getElementById("delete");
+del.addEventListener("click",delTarea);
+function delTarea(event) {
+    let t = document.getElementById("task");
+    let lTask = t.lastChild;
+    t.removeChild(lTask);
+}
